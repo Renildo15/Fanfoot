d46bf7d3e1cf5667180a35ed2cedeb810e211050
Renildo15 <renildorabi@ufrn.edu.br>
2025-10-26 13:25:11 -0300

    feat: exibindo jogadores

--- app/components/editor/tables/players_table.py
+++ app/components/editor/tables/players_table.py
@@ -1,7 +1,33 @@
 import flet as ft
+from app.db.models import Player
+from app.services.club_service import get_club
+from typing import List
+from app.utils.get_position import get_position
 
 
-def players_table():
+def players_table(players: List[Player]):
+    rows = []
+    for player in players:
+
+        if player.current_club_id:
+            club = get_club(player.current_club_id)
+            club_name = club.name
+        else:
+            club_name = "Livre"
+
+        rows.append(
+            ft.DataRow(
+                cells=[
+                    ft.DataCell(ft.Text(player.full_name)),
+                    ft.DataCell(ft.Text(str(player.age))),
+                    ft.DataCell(ft.Text(get_position(player.position.value))),
+                    ft.DataCell(ft.Text(str(player.overall))),
+                    ft.DataCell(ft.Text(str(player.potential))),
+                    ft.DataCell(ft.Text(club_name)),
+                ]
+            )
+        )
+
     return ft.DataTable(
         columns=[
             ft.DataColumn(ft.Text("Nome")),
@@ -11,5 +37,5 @@ def players_table():
             ft.DataColumn(ft.Text("Potencial")),
             ft.DataColumn(ft.Text("Clube")),
         ],
-        rows=[],
+        rows=rows,
     )
